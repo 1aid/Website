4e69afdd589a5f5ea712e2130838805435d123dc
1aid <aidenbrown@usf.edu>
2023-07-31 19:15:27 -0400

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,173 @@
+const logotext = "AIDEN";
+const meta = {
+    title: "Aiden Brown",
+    description: "I’m Aiden Brown, a Solution Architect and Full stack devloper",
+};
+
+const introdata = {
+    title: "Hello, I’m Aiden Brown",
+    animated: {
+        first: "Experienced Cloud Architect",
+        second: "AWS Certified",
+        third: "Experienced Developer",
+    },
+    description: "I am a versatile and skilled Solution Architect with a strong background in AWS environments. With extensive experience in both development and architecture, I bring a unique perspective to solving complex challenges. As a seasoned developer, I am fluent in a wide range of programming languages, including Python, JavaScript, React, Node.js, HTML, CSS, Flask, Java, and C. My expertise extends to database management, enabling me to design efficient and scalable solutions. Throughout my career, I have demonstrated a passion for delivering innovative and robust solutions that exceed expectations. My commitment to staying updated with the latest technologies ensures that I am always at the forefront of the ever-evolving tech landscape.",
+    your_img_url: "https://www.linkpicture.com/q/IMG_5389.jpg",
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "My name is Aiden Brown, I am from Tampa, FL.  I possess a passionate drive to expand my expertise in technology, with a specific focus on cloud architecture and development. Already equipped with considerable expertise in these domains, I am now seeking promising opportunities to propel my career to new heights and contribute my skills to innovative projects. I am eager to embrace challenges, grow my knowledge, and make meaningful contributions to the ever-evolving world of cloud technology.",
+};
+const worktimeline = [{
+        jobtitle: "",
+        where: "",
+        date: "",
+    },
+    {
+        jobtitle: "",
+        where: "",
+        date: "",
+    },
+    {
+        jobtitle: "",
+        where: "",
+        date: "",
+    },
+];
+
+const skills = [
+    {
+        name: "AWS",
+        value: 70,
+    },
+    {
+        name: "Azure",
+        value: 20,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 65,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "Java",
+        value: 35,
+    },
+];
+
+const services = [{
+        title: "Cloud Architecture",
+        description: "As an experienced AWS Solution Architect, I possess in-depth expertise across all AWS services. I am well-versed in architecting and deploying scalable cloud solutions. With a strong grasp of serverless architecture, IAM, and networking tools like VPC, I can deliver secure, efficient, and cost-effective solutions tailored to diverse project needs.",
+    },
+    {
+        title: "Databases",
+        description: "I have extensive expertise in deploying and managing Amazon RDS (Relational Database Service) instances, which include popular database engines like MySQL, PostgreSQL, SQL Server, and Oracle. Moreover, I am well-versed in leveraging Amazon DynamoDB for NoSQL database needs, providing fast and flexible document and key-value data storage.",
+    },
+    {
+        title: "Programming",
+        description: "I possess a comprehensive understanding of the programming languages commonly used within the AWS ecosystem. I am fluent in key languages such as Python, JavaScript, Java, and Node.js, enabling me to develop versatile and efficient applications that seamlessly integrate with AWS services. Additionally, my proficiency in languages like C and React allows me to build robust solutions that cater to specific project requirements, ensuring optimal performance and reliability within the AWS environment.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aiden.brownwebsite@gmail.com",
+    YOUR_FONE: "(727)-470-4701",
+    description: "",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/aiden-brown-98724225a/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
